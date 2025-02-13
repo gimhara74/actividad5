1adf80bc9627f3382f3ff5ba9ef3fc800a9f48eb
Juan Rodriguez <gimhara@hotmail.com>
2025-02-14 00:59:24 +0100

    Crear interfaz, json, añadir elementos del array al html

--- blog/src/app/components/formulario/formulario.component.ts
+++ blog/src/app/components/formulario/formulario.component.ts
@@ -1,15 +1,44 @@
 import { Component } from '@angular/core';
+import { IPublicacion } from '../../interfaces/ipublicacion.interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  arrPublicaciones: IPublicacion[] = [
+    {
+      titulo: 'Noticia 1',
+      imagen: 'https://via.placeholder.com/150',
+      noticia: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in turpis nec dui tincidunt mollis. Nullam nec sapien nec libero lacinia tincidunt. Nullam in turpis nec dui tincidunt mollis. Nullam nec sapien nec libero lacinia tincidunt.',
+      fecha: new Date()
+    },
+    {
+      titulo: 'Noticia 2',
+      imagen: 'https://via.placeholder.com/150',
+      noticia: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in turpis nec dui tincidunt mollis. Nullam nec sapien nec libero lacinia tincidunt. Nullam in turpis nec dui tincidunt mollis. Nullam nec sapien nec libero lacinia tincidunt.',
+      fecha: new Date()
+    },
+    {
+      titulo: 'Noticia 3',
+      imagen: 'https://via.placeholder.com/150',
+      noticia: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in turpis nec dui tincidunt mollis. Nullam nec sapien nec libero lacinia tincidunt. Nullam in turpis nec dui tincidunt mollis. Nullam nec sapien nec libero lacinia tincidunt.',
+      fecha: new Date()
+    }
+  ];
   
-  clickheader():void {
-    console.log('click en header JHONNY 16');
+  ngOnInit() {
+    console.log(this.arrPublicaciones);
+  }
+  clickheader() {
+
+    
+    //alert('Has hecho clic en el header');
+    
+    // Aquí puedes agregar la lógica que desees ejecutar cuando se haga clic en el botón
   }
 
 
---
+++ blog/src/app/interfaces/ipublicacion.interfaces.ts
@@ -0,0 +1,6 @@
+export interface IPublicacion {
+    titulo: string;
+    imagen: string;
+    noticia: string;
+    fecha: Date;
+}
